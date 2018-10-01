0eda2dbb899424ab0f7757cc1e5c50c9feef44c6
MDBarbier <matthew-7.7@hotmail.com>
2018-10-01 19:10:26 +0100

    removed asp.net typescript demo

--- typescriptdemo/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="typescriptdemo.MvcApplication" Language="C#" %>
--- typescriptdemo/Scripts/TypeScript/file1.ts
+++
@@ -1,9 +0,0 @@
-﻿alert("Hello world!");
-
-var el: HTMLElement = document.getElementById('changeMe');
-
-var type = (el instanceof HTMLElement) ? "HTMLElement" : "Unknown";
-
-var message = "This element has been located and altered with TypeScript, and it has typeof: " + type.toString(); 
-el.innerText = message;
-
